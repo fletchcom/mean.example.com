f90276c3b14539763d51e2658788fca94597c40f
laurafletcher <laura@fletchcom.net>
2020-10-30 14:33:23 -0500

    Add Passport to the users model

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var uniqueValidator = require('mongoose-unique-validator');
-
+var passportLocalMongoose = require('passport-local-mongoose'); 
 //Create a schema
 var Users = new Schema({
   email: {
@@ -51,3 +51,5 @@ Users.pre('save', function(next){
 Users.plugin(uniqueValidator);
 
 module.exports  = mongoose.model('Users', Users);
+
+Users.plugin(passportLocalMongoose);
