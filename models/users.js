ac633e50b023218583ee6e94fa85babf2f1c6e1e
laurafletcher <laura@fletchcom.net>
2020-10-30 14:17:29 -0500

    Auto-update the modified date on edit

--- models/users.js
+++ models/users.js
@@ -29,7 +29,10 @@ var Users = new Schema({
   }
 }
 });
-
+Users.pre('save', function(next){
+  this.modified = new Date().toISOString();
+  next();
+});
 //Add unique validation properties to the model
 Users.plugin(uniqueValidator);
 
