8503d829fbd4eca44812e617b1dcf4bfe46fb9d0
laurafletcher <laura@fletchcom.net>
2020-11-04 11:37:28 -0600

    Get the data for the selected user

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -143,6 +143,25 @@ var usersApp = (function() {
       app.innerHTML = card;
     }
   }  
+  function editUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+  
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+  
+    xhr.send();
+  
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      console.log(data);
+    }
+  }  
+
   function postRequest(formId, url){
     let form = document.getElementById(formId);
     form.addEventListener('submit', function(e){
@@ -189,7 +208,7 @@ var usersApp = (function() {
           break;
     
         case '#edit':
-          console.log('EDIT');
+          editUser(hashArray[1]);
           break;
     
         case '#delete':
