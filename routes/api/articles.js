5a465ea2ab6769f856974e585f2d85138d73ca85
laurafletcher <laura@fletchcom.net>
2020-11-08 14:23:46 -0600

    Implement a route for api/users

---
+++ routes/api/articles.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var Articles = require('../../models/articles');
+
+router.get('/', function(req, res, next) {
+    res.json({success: true});
+  });
+
+router.get('/:articleId', function(req,res){});
+
+router.post('/', function(req, res) {});
+
+router.put('/', function(req, res){});
+
+router.delete('/:articleId', function(req,res){});
+
+module.exports = router;
