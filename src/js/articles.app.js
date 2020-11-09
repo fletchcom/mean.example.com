09f7bf64427692812bfdebc63a569354cc8b68e9
laurafletcher <laura@fletchcom.net>
2020-11-09 16:42:32 -0600

    Attempt to display articles in HTML table

--- src/js/articles.app.js
+++ src/js/articles.app.js
@@ -14,16 +14,57 @@ var ArticlesApp = (function() {
     xhr.send();
 
     xhr.onload = function(){
+      let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
-      console.log(data);
-    }
+      let articles = data.articles;
+      let table = '';
+      let rows = '';
+
+  //Loop each article into its own HTML table row, each article should
+  //have a link an article view
+  for (let i=0; i<articles.length; i++) {
+    rows = rows + `<tr>
+      <td>
+        <a href="#view-${articles[i]['_id']}">${articles[i]['title']}, ${articles[i]['description']}</a>
+      </td>
+      <td>${articles[i]['keywords']}</td>
+      <td>${articles[i]['body']}</td>
+    </tr>`;
   }
 
-  return {
-    load: function(){
-      viewArticles();
-    }
-  }
+  //Create an articles panel, add a table to the panel, inject the rows into the
+  //table
+  table = `<div class="card">
+    <div class="card-header clearfix">
+      <h2 class="h3 float-left">Users</h2>
+      <div class="float-right">
+        <a href="#create" class="btn btn-primary">New Article</a>
+      </div>
+    </div>
+    <div class="table-responsive">
+      <table class="table table-striped table-hover table-bordered">
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Keywords</td>
+          </tr>
+        </thead>
+        <tbody>${rows}</tbody>
+      </table>
+    </div>
+  </div>`;
+
+//Append the HTML to the #app
+app.innerHTML = table;
+}
+}
+
+return {
+load: function(){
+  viewArticles();
+}
+}
 
 })();
 
