28cdccd40a7717f94063c730f8cfff91d4061314
laurafletcher <laura@fletchcom.net>
2020-11-09 13:52:27 -0600

    Added update an article function

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -14,13 +14,71 @@ router.get('/', function(req, res, next) {
 router.get('/:articleId', function(req,res){
   
     var articleId = req.params.articleId;
-     Articles.findOne({'_id':articleId}, function(err, user){
+     Articles.findOne({'_id':articleId}, function(err, article){
        if(err){
         return res.json({'success':false, 'error': err});
       }
-       return res.json({'success':true, 'user': user});
+       return res.json({'success':true, 'article': article});
      });
    });
   
+   router.post('/', function(req, res) {
+    Articles.create(new Articles({
+      title: req.body.title,
+      description: req.body.description,
+      keywords: req.body.keywords,
+      body: req.body.body
+    }), function(err, article){
+      
+      if(err){
+        return res.json({success: false, article: req.body, error: err});
+      }
+  
+      return res.json({success: true, article: article});
+      
+    });
+  });  
+  
+  router.put('/', function(req, res){
+
+    Articles.findOne({'_id': req.body._id}, function(err, article){
+  
+     if(err) {
+       return res.json({success: false, error: err});
+     }
+  
+     if(article) {
+  
+      let data = req.body;
+  
+      if(data.title){
+        article.title = data.title;
+      };
+  
+      if(data.description){
+      article.description = data.description;
+      };
+  
+      if(data.keywords){
+      article.keywords = data.keywords;
+      };
+  
+      if(data.body){
+      article.body = data.body;
+      };
+  
+      article.save(function(err){
+        if(err){
+          return res.json({success: false, error: err});
+        }else{
+          return res.json({success: true, article:article});
+        }
+      });
+  
+     }
+  
+    });
+    
+  });
 
 module.exports = router;
