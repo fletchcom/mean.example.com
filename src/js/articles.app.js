9ebb53f7403aecfbfccbfd6bcf1909d37353fd13
laurafletcher <laura@fletchcom.net>
2020-11-09 16:06:49 -0600

    Pull a list of articles from the API

---
+++ src/js/articles.app.js
@@ -0,0 +1,30 @@
+var ArticlesApp = (function() {
+
+  function viewArticles(){
+
+    let uri = `${window.location.origin}/api/articles`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      console.log(data);
+    }
+  }
+
+  return {
+    load: function(){
+      viewArticles();
+    }
+  }
+
+})();
+
+articlesApp.load();
