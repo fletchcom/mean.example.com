a810581a8af296784731e9889faf671c4c222773
laurafletcher <laura@fletchcom.net>
2020-11-09 11:18:58 -0600

    Retrieve a single article by _id

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -11,4 +11,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:articleId', function(req,res){
+  
+    var articleId = req.params.articleId;
+     Articles.findOne({'_id':articleId}, function(err, user){
+       if(err){
+        return res.json({'success':false, 'error': err});
+      }
+       return res.json({'success':true, 'user': user});
+     });
+   });
+  
+
 module.exports = router;
