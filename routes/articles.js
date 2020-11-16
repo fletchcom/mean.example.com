6f3a7b5658061d1154fda4b73b965da526087a3b
laurafletcher <laura@fletchcom.net>
2020-11-16 17:21:40 -0600

    Yet another failed attempt at an article CRUD

--- routes/articles.js
+++ routes/articles.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var Articles = require('../models/articles');
 var today = new Date();
+
 router.get('/app', function(req, res, next) {
   res.render('articles/app', { title: 'Article Management' });
 });
+
 router.get('/', function(req, res, next) {
   Articles.find({},function(err, articles){
     console.log(articles);
@@ -15,6 +17,7 @@ router.get('/', function(req, res, next) {
       return res.render('articles/index', { title: 'Articles' , articles:articles});
   });
 });
+
 router.get('/:slug', function(req, res, next) {
   Articles.findOne({'slug':req.params.slug},function(err, articles){
     if(err)
@@ -27,4 +30,5 @@ router.get('/:slug', function(req, res, next) {
   }
   });
 });
+
 module.exports = router;
